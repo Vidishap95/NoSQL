cb79c264221f271d211be257fec94b176d7f5c75
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-06-01 22:43:02 -0400

    servr file

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-const express = require()
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// setting environment variables
+const PORT = process.env.Port || 3001;
+const app = express();
+
+// use middlewares to parse incoming data
+app.use(express.urlencoded ({ extended: true }));
+app.use(express.json());
+
+//use routes defined in routes.js
+app.use(routes);
+
+//connect to mongodb database and start server
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port${PORT}!`);
+    });
+});
